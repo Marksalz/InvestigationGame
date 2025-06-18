e22f78bed733996ce4a442aff357e80b5a729c7e
Mark <menachemsalzberg13@gmail.com>
2025-06-18 13:37:16 +0300

    Implement ISensorBroken interface for sensor management
    Added the `ISensorBroken` interface to handle broken sensors, including methods for activation and state management. Updated `MotionSensor` and `PulseSensor` classes to implement this interface, modifying the activation logic and state tracking.
    
    Refactored agent classes to simplify weakness matching using `FindIndex` and `RemoveAt`.
    
    Included commented-out code in `InvestigationManager.cs` for future handling of broken sensors, enhancing overall functionality and maintainability of the sensor system.

---
+++ InvestigationGame/Interface/ISensorBroken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.Interface
+{
+    public interface ISensorBroken
+    {
+        bool IsBroken { get; set; }
+        void Activate();
+        void ResetActivation();
+    }
+}
--- InvestigationGame/Sensors/MotionSensor.cs
+++ InvestigationGame/Sensors/MotionSensor.cs
@@ -3,18 +3,42 @@ using System;
 
 namespace InvestigationGame.Sensors
 {
-    public class MotionSensor : ISensor
+    public class MotionSensor : ISensor, ISensorBroken
     {
         public string Name { get; } = "motion";
-        private int _activations = 0;
+        private static int _activations = 0;
         private const int MaxActivations = 3;
-        public bool IsBroken => _activations >= MaxActivations;
+        public bool IsBroken { get; set; } = false; 
 
-        public bool Matches(InvestigationGame.Enums.SensorType weakness)
+        public bool Matches(Enums.SensorType weakness)
         {
             if (IsBroken) return false;
+            //_activations++;
+            return weakness == Enums.SensorType.Motion;
+        }
+
+        public void Activate()
+        {
+            if (IsBroken)
+            {
+                Console.WriteLine($"Activated: {_activations} times.");
+                return;
+            }
             _activations++;
-            return weakness == InvestigationGame.Enums.SensorType.Motion;
+            if (_activations >= MaxActivations)
+            {
+                IsBroken = true;
+                Console.WriteLine($"Activated: {_activations} times.");
+            }
+            else
+            {
+                Console.WriteLine($"Activated: {_activations} times.");
+            }
+        }
+
+        public void ResetActivation()
+        {
+            _activations = 0;
         }
     }
 }
